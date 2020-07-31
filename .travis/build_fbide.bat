48754e356be7e3ff52ff0dc23f91160f10338d33
Albert Varaksin <albeva@me.com>
2020-08-01 00:34:21 +0100

    Fix paths

--- .travis/build_fbide.bat
+++ .travis/build_fbide.bat
@@ -5,8 +5,8 @@ mkdir fbide_build
 cmake -G "Ninja" ^
       --parallel ^
       -DCMAKE_BUILD_TYPE=Release ^
-      -DWXWIN= "%cd%\libs\wxWidgets\dist" ^
-      -DWX_SOURCE_DIR="%cd%\libs\wxWidgets\wxWidgets-3.1.4" ^
+      -DWXWIN= "%cd%\wx-dist" ^
+      -DWX_SOURCE_DIR="%cd%\wx-src" ^
       -S fbide -B fbide_build
 
 cd fbide_build
