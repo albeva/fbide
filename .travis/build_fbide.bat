cb04584b0018c8bd6d1783d2d2ddd6f58f92b30c
Albert Varaksin <albeva@me.com>
2020-08-01 00:51:00 +0100

    fix env paths

--- .travis/build_fbide.bat
+++ .travis/build_fbide.bat
@@ -1,4 +1,4 @@
-call %~dp0dev_cmd.bat
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x86
 
 mkdir fbide_build
 
--- .travis/dev_cmd.bat
+++
@@ -1,6 +0,0 @@
-SET CXX=""
-SET CXX_FOR_BUILD=""
-SET CC=""
-SET CC_FOR_BUILD=""
-
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x86
