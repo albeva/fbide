93dca05df8ca2a1d24eca6e21424182f08050925
Albert Varaksin <albeva@me.com>
2020-08-01 03:06:06 +0100

    try forcing library

--- .travis/build_fbide.sh
+++ .travis/build_fbide.sh
@@ -2,6 +2,8 @@ mkdir fbide_build
 
 cmake -G "NMake Makefiles" \
       -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded \
+      -DYAML_MSVC_SHARED_RT=OFF \
       -DWXWIN="$(pwd)/wx-dist" \
       -DWX_SOURCE_DIR="$(pwd)/wx-src" \
       -S . -B fbide_build
