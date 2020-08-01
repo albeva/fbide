3d24e961318785bb6dac265d91d03af8781db443
Albert Varaksin <albeva@me.com>
2020-08-01 02:23:02 +0100

    Fix paths and revert to using env override trick to force msvc compiler

--- .travis/build_fbide.sh
+++ .travis/build_fbide.sh
@@ -3,8 +3,8 @@ mkdir fbide_build
 cmake -G "Ninja" \
       --parallel \
       -DCMAKE_BUILD_TYPE=Release \
-      -DWXWIN= "%cd%\wx-dist" \
-      -DWX_SOURCE_DIR="%cd%\wx-src" \
+      -DWXWIN="$(pwd)/wx-dist" \
+      -DWX_SOURCE_DIR="$(pwd)/wx-src" \
       -S . -B fbide_build
 
 cd fbide_build
