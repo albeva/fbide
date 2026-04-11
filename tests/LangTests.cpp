396b1bdba40a3919a47c26af8030d2056db6b0c0
Albert Varaksin <albeva@me.com>
2026-04-11 15:02:29 +0100

    Add translation system with legacy .fbl file import
    LangId enum maps all 245 legacy numeric indexes to named constants.
    Lang class loads .fbl files via wxFileConfig, stores strings in a
    vector for direct index access. Trims trailing whitespace from
    legacy files on load. Free/unused IDs grouped at end of enum.

---
+++ tests/LangTests.cpp
@@ -0,0 +1,70 @@
+//
+// FBIde editor for FreeBASIC - https://freebasic.net
+// Copyright (c) 2026 Albert Varaksin
+// Licensed under the MIT License. See LICENSE file for details.
+// https://github.com/albeva/fbide
+//
+#include "lib/config/Lang.hpp"
+#include <gtest/gtest.h>
+
+using namespace fbide;
+
+// Path to legacy language files from the original FBIde project
+static const wxString testDataPath = "/Users/albert/Developer/fbide/bin/IDE/lang/";
+
+TEST(LangTests, LoadEnglish) {
+    Lang lang;
+    lang.load(testDataPath + "english.fbl");
+
+    EXPECT_EQ(lang[LangId::Welcome], "Welcome to FBIde!");
+    EXPECT_EQ(lang[LangId::OK], "&OK");
+    EXPECT_EQ(lang[LangId::Cancel], "&Cancel");
+    EXPECT_EQ(lang[LangId::MenuFile], "&File");
+    EXPECT_EQ(lang[LangId::FileNew], "&New ..");
+    EXPECT_EQ(lang[LangId::EditUndo], "&Undo");
+    EXPECT_EQ(lang[LangId::RunCompile], "Compile");
+    EXPECT_EQ(lang[LangId::ToolbarNew], "New file (Ctrl+N)");
+    EXPECT_EQ(lang[LangId::Untitled], "Untitled");
+    EXPECT_EQ(lang[LangId::SaveChanges], "Save changes?");
+}
+
+TEST(LangTests, LoadGerman) {
+    Lang lang;
+    lang.load(testDataPath + "deutsch.fbl");
+
+    EXPECT_EQ(lang[LangId::MenuFile], "&Datei");
+    EXPECT_FALSE(lang[LangId::EditUndo].empty());
+    EXPECT_FALSE(lang[LangId::FileQuit].empty());
+}
+
+TEST(LangTests, MissingKeyReturnsEmpty) {
+    Lang lang;
+    lang.load(testDataPath + "english.fbl");
+
+    // Index 0 is not used in the language files
+    EXPECT_EQ(lang.get(static_cast<LangId>(999)), "");
+}
+
+TEST(LangTests, Reload) {
+    Lang lang;
+    lang.load(testDataPath + "english.fbl");
+    EXPECT_EQ(lang[LangId::MenuFile], "&File");
+
+    lang.load(testDataPath + "deutsch.fbl");
+    EXPECT_EQ(lang[LangId::MenuFile], "&Datei");
+}
+
+TEST(LangTests, Clear) {
+    Lang lang;
+    lang.load(testDataPath + "english.fbl");
+    EXPECT_EQ(lang[LangId::Welcome], "Welcome to FBIde!");
+
+    lang.clear();
+    EXPECT_EQ(lang[LangId::Welcome], "");
+}
+
+TEST(LangTests, NonexistentFile) {
+    Lang lang;
+    lang.load("/nonexistent/path.fbl");
+    EXPECT_EQ(lang[LangId::Welcome], "");
+}
