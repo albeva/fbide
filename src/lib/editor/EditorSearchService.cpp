5b1a2cf15db64663fd6f32f770ba1db1778aae49
Albert Varaksin <albeva@me.com>
2026-05-25 22:22:46 +0100

    Scaffold EditorSearchService
    Empty wxEvtHandler subclass; takes Context& in ctor (unused for now).
    Subsequent steps fill in the find / replace / goto-line surface lifted
    from DocumentManager and wire it into Context.

---
+++ src/lib/editor/EditorSearchService.cpp
@@ -0,0 +1,10 @@
+//
+// FBIde editor for FreeBASIC - https://freebasic.net
+// Copyright (c) 2026 Albert Varaksin
+// Licensed under the MIT License. See LICENSE file for details.
+// https://github.com/albeva/fbide
+//
+#include "EditorSearchService.hpp"
+using namespace fbide;
+
+EditorSearchService::EditorSearchService(Context& /*ctx*/) {}
